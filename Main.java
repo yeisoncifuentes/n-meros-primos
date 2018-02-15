f4086b3bcdc6e886a6f9c3518235459f47346127
Unknown <yeisoncifuentes2@gmail.com>
2018-02-15 17:17:36 -0500

    primera validacion
    se crea un for para recorrer el arreglo de 4 espacios, tambien se valida que los tres primeros numeros tanto negativos como positivos son primos

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main{
+
+	public static void main(String[] arg){
+		System.out.println("");
+		System.out.println("clase de programacion II");
+		System.out.println("EJERCICIO NUMERO PRIMOS");
+		System.out.println("");
+		System.out.println("\n\n\t\t");
+		System.out.println("");
+		System.out.println("");
+		
+		int primos[] = {-7,-2,1,99};		
+		
+	    for (int i=0; i<4;i+=1){
+               if(primos[i]>=-3 && primos[i]<=3 ){
+                   primos[i]=1;
+		}else{
+			
+                    }
+	    }
+	    
+        
+	}
+}
